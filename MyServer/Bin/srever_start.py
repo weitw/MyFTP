28b473cdd64bf08646f8a024af69cbdae7563c4a
pyexile <41028506+pyexile@users.noreply.github.com>
2018-09-28 16:32:57 +0800

    Create srever_start.py

---
+++ MyServer/Bin/srever_start.py
@@ -0,0 +1,19 @@
+# --*-- coding:utf-8 --*--
+
+import sys
+import os
+import json
+
+Base_dir = os.path.dirname(os.getcwd())  # 目录为MyFTP\MyServer
+sys.path.append(Base_dir)  # 将目录添加到环境变量，这样就可以不用来回切换路径了
+print(Base_dir)
+from server_code.code import MyTCPHandler
+
+
+if __name__ == "__main__":
+    try:
+        s = MyTCPHandler()
+        s.handle()
+    except Exception as e:
+        print("运行程序出错：", e)
+
