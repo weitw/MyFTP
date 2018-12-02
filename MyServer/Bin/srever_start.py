e619aa0120a87555d64a10e74137b2402d7f2a56
pyexile <41028506+pyexile@users.noreply.github.com>
2018-12-02 12:23:37 +0800

    Update srever_start.py

--- MyServer/Bin/srever_start.py
+++ MyServer/Bin/srever_start.py
@@ -1,13 +1,14 @@
+# /bin/python3/bin/python3.6
 # --*-- coding:utf-8 --*--
+# the start of server
 
 import sys
 import os
-import json
+from ..server_code.code import MyTCPHandler
 
 Base_dir = os.path.dirname(os.getcwd())  # 目录为MyFTP\MyServer
 sys.path.append(Base_dir)  # 将目录添加到环境变量，这样就可以不用来回切换路径了
-print(Base_dir)
-from server_code.code import MyTCPHandler
+# print("The current directory is : ", Base_dir)
 
 
 if __name__ == "__main__":
