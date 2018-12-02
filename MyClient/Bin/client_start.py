e3ab22232ac61a4fa1e6dbed2b74c25e47d62e14
pyexile <41028506+pyexile@users.noreply.github.com>
2018-12-02 12:20:40 +0800

    Update client_start.py

--- MyClient/Bin/client_start.py
+++ MyClient/Bin/client_start.py
@@ -1,11 +1,14 @@
+# /bin/python3/bin/python3.6
 # --*-- coding:utf-8 --*--
+# the start of client
 
 import os
 import sys
+from ..client_code.code import Client
 
 BaseDir = os.path.dirname(os.getcwd())  # 目录为MyFTP/MyClient
 sys.path.append(BaseDir)
-from client_code.code import Client
+
 
 if __name__ == '__main__':
     c = Client()
